c923a472fbeb2e7cdc6a0fa3245f7d66dd8e7cbd
Frank Song <ysong71@illinois.edu>
2017-05-30 11:26:51 -0400

    20170530112651

--- failure_detector_mp2/detector.cpp
+++ failure_detector_mp2/detector.cpp
@@ -94,7 +94,7 @@ void* detector::run(){
 			for(auto m:alivemembers){
 
 				network_udp::generate_msg(msg_send_buffer,msg_t::PING,m.c_str());
-				network_udp::send_msg(msg_send_buffer,BUFFER_SIZE,DETECTORPORT,m.c_str());
+				network_udp::send_msg(msg_send_buffer,BUFFER_SIZE,SERVERPORT,m.c_str());
 				// network_udp::send_msg(msg_t::PING,SERVERPORT,m.c_str());
 				
 				#if DEBUG
