93157f07ba7fc5bc696895316490b27bec49e47e
Frank Song <ysong71@illinois.edu>
2017-07-12 22:16:22 -0400

    20170712221622

--- distributed_file_system_mp3/file_server.h
+++ distributed_file_system_mp3/file_server.h
@@ -14,7 +14,7 @@
 // #include <sys/mman.h>
 
 #define DUPLICATE_COUNT 3
-const string map_in_file="file_add";
+const string map_in_file="files.store";
 class file_server:public Thread{
 public:
 	file_server(loggerThread *lg,alive_member *am);
